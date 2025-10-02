9dbb5db303b07c8d31334ac09d383123fdb57cd8
mostafa <mostafayounis630@gmail.com>
2025-10-02 10:14:54 +0300

    delete some users files

--- Skill_Exchange.Application/Services/User/Queries/GetUserById.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using Skill_Exchange.Application.DTOs;
-using Skill_Exchange.Application.DTOs.User;
-
-namespace Skill_Exchange.Application.Services.User.Queries
-{
-    public record GetUserById(string id) : IRequest<Result<UserDTO>>;
-}
