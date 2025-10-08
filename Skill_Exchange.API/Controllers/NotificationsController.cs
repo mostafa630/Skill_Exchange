bbab3e29438502dc286006762f6ebcce0c54e093
Farag_Elyan <faragelyan722@gmail.com>
2025-10-08 04:26:53 +0300

    start in notifications service and finish update

---
+++ Skill_Exchange.API/Controllers/NotificationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Skill_Exchange.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+    }
+}
---
+++ Skill_Exchange.Application/DTOs/Notifications/UpdateNotificationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill_Exchange.Application.DTOs.Notifications
+{
+    internal class UpdateNotificationDto
+    {
+    }
+}
